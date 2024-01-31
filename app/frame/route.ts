f670e8be7fa4dcbb5db68fb9e7c4596580ec9d08
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-30 21:32:29 -0600

    Update route.ts

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -7,6 +7,24 @@ export async function POST(request: NextRequest) {
 
   const n = Number(new URL(request.url).searchParams.get("n") as string);
 
+  if (n === 1 && button === 1) {
+    return new NextResponse(
+      `
+                        <!DOCTYPE html>
+                        <html>
+                          <head>
+                                <meta property="fc:frame" content="vNext" />
+                                <meta property="fc:frame:image" content="https://motherfuckingframe.com/2.png" />
+                                <meta property="fc:frame:button:1" content="Back" />
+                                <meta property="fc:frame:button:2" content="Next" />
+                                <meta property="fc:frame:post_url" content="https://motherfuckingframe.com/frame?n=2" />
+                          </head>
+                        </html>
+                        `,
+      { status: 200, headers: { "content-type": "text/html" } }
+    );
+  }
+
   if (n === 2 && button === 1) {
     return new NextResponse(
       `
